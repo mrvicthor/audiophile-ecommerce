2ea7554caa024b30e0f37ca5cbc7f8d3f9a153bb
mrvicthor <victoreleanya89@gmail.com>
2022-11-13 08:52:41 +0000

    updated styling

--- components/speaker/index.tsx
+++ components/speaker/index.tsx
@@ -4,7 +4,7 @@ const Speaker = () => {
     <section className="mt-[121px]  px-6">
       <div className="bg-[#d87d4a] h-[600px] overflow-hidden relative rounded">
         <div className="outer__circle h-[558px] w-[558px] border-[#d8d8d8] border absolute bottom-[163px] left-[50%] -translate-x-[275px]" />
-        <div className="inner__circle border-[#d8d8d8] border h-[20.4375rem] w-[20.4375rem] rounded-[50%] absolute" />
+        <div className="inner__circle border-[#d8d8d8] border h-[20.4375rem] w-[20.4375rem] rounded-[50%] absolute left-[50%] -translate-x-[50%]" />
         <div className="inner__circle_child h-[279px] w-[279px] border-[#d8d8d8] border-[0.5px] absolute top-[21px] left-[50%] -translate-x-[50%]" />
 
         <div className="h-[207px] w-[172.25px] absolute top-[57px] left-[50%] -translate-x-[50%]">
