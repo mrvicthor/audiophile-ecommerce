eae52908a616847307ad88d5ef03e4c710a07e8d
mrvicthor <victoreleanya89@gmail.com>
2022-11-25 20:37:53 +0000

    updated text

--- components/title/index.tsx
+++ components/title/index.tsx
@@ -28,11 +28,7 @@ const Title = ({ title, children, description, isNew }: TitleProps) => {
           </h2>
         )}
         <p className="font-bold text-[1.75rem] leading-[2.39rem] tracking-[1px] article_title text-center text-[#000000] uppercase  lg:text-left lg:text-[2.5rem]">
-          {strArr.length > 2
-            ? `${strArr[0]} ${strArr[1]} ${strArr[2]}`
-            : strArr[0]}
-
-          <span className="block">{strArr.slice(-1)}</span>
+          {title}
         </p>
         <p className="font-medium text-[0.9375rem] leading-[1.5625rem] text-center text-[#000000] opacity-50 lg:text-left">
           {description}
