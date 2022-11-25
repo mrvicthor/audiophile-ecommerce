87b7f270c44d25184c04157b27a27374b12bd9ac
mrvicthor <victoreleanya89@gmail.com>
2022-11-25 20:55:57 +0000

    updates

--- pages/headphones.tsx
+++ pages/headphones.tsx
@@ -9,31 +9,32 @@ const Headphone = () => {
         <section className="px-6 mt-[4rem] md:mt-[7.5rem] lg:mt-[10rem]">
           <div className="space-y-[7.5rem] pt-16 lg:max-w-[69.364rem] lg:mx-auto category overflow-hidden">
             {headphones.map((headphone) => (
-              <Title
-                key={headphone.id}
-                title={headphone.name}
-                description={headphone.description}
-                isNew={headphone.new}
-              >
-                <Image
-                  src={headphone.categoryImage.mobile}
-                  alt={headphone.name}
-                  fill
-                  className="md:hidden object-cover"
-                />
-                <Image
-                  src={headphone.categoryImage.tablet}
-                  alt={headphone.name}
-                  fill
-                  className="hidden md:block lg:hidden object-cover"
-                />
-                <Image
-                  src={headphone.categoryImage.desktop}
-                  alt={headphone.name}
-                  fill
-                  className="hidden lg:block object-cover"
-                />
-              </Title>
+              <h1>headphone</h1>
+              // <Title
+              //   key={headphone.id}
+              //   title={headphone.name}
+              //   description={headphone.description}
+              //   isNew={headphone.new}
+              // >
+              //   <Image
+              //     src={headphone.categoryImage.mobile}
+              //     alt={headphone.name}
+              //     fill
+              //     className="md:hidden object-cover"
+              //   />
+              //   <Image
+              //     src={headphone.categoryImage.tablet}
+              //     alt={headphone.name}
+              //     fill
+              //     className="hidden md:block lg:hidden object-cover"
+              //   />
+              //   <Image
+              //     src={headphone.categoryImage.desktop}
+              //     alt={headphone.name}
+              //     fill
+              //     className="hidden lg:block object-cover"
+              //   />
+              // </Title>
             ))}
           </div>
         </section>
