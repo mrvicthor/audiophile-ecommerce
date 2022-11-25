461b826d3fa4b29119697afcfbf440944540ad24
mrvicthor <victoreleanya89@gmail.com>
2022-11-25 21:55:06 +0000

    updated

--- components/title/index.tsx
+++ components/title/index.tsx
@@ -27,7 +27,7 @@ const Title = ({ title, children, description, isNew }: TitleProps) => {
             New Product
           </h2>
         )}
-        <p className="font-bold text-[1.75rem] leading-[2.39rem] tracking-[1px] article_title text-center border border-red-400 text-[#000000] uppercase  lg:text-left lg:text-[2.5rem]">
+        <p className="font-bold text-[1.75rem] leading-[2.39rem] tracking-[1px] article_title text-center text-[#000000] uppercase  lg:text-left lg:text-[2.5rem]">
           {strArr.length > 2
             ? `${strArr[0]} ${strArr[1]} ${strArr[2]}`
             : `${strArr[0]}`}{" "}
