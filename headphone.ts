2293f94b89aa7abb9e1a66f906d8e27cd8fad7d7
mrvicthor <victoreleanya89@gmail.com>
2022-11-26 06:44:51 +0000

    updated

--- headphone.ts
+++ headphone.ts
@@ -1,27 +1,5 @@
 export const headphones = [
   {
-    id: 3,
-    slug: "xx99-mark-one-headphones",
-    name: "XX99 Mark I Headphones",
-    image: {
-      mobile: "/images/image-product-xx99-mark-one-mobile.jpg",
-      tablet: "/images/image-product-xx99-mark-one-tablet.jpg",
-      desktop: "/images/image-product-xx99-mark-one-desktop.jpg",
-    },
-    category: "headphones",
-    categoryImage: {
-      mobile: "/images/image-category-page-preview-xx99-mark-one-mobile.jpg",
-      tablet: "/images/image-category-page-preview-xx99-mark-one-tablet.jpg",
-      desktop: "/images/image-category-page-preview-xx99-mark-one-desktop.jpg",
-    },
-    new: false,
-    price: 1750,
-    description:
-      "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
-    features:
-      "As the headphones all others are measured against, the XX99 Mark I demonstrates over five decades of audio expertise, redefining the critical listening experience. This pair of closed-back headphones are made of industrial, aerospace-grade materials to emphasize durability at a relatively light weight of 11 oz.\n\nFrom the handcrafted microfiber ear cushions to the robust metal headband with inner damping element, the components work together to deliver comfort and uncompromising sound. Its closed-back design delivers up to 27 dB of passive noise cancellation, reducing resonance by reflecting sound to a dedicated absorber. For connectivity, a specially tuned cable is includes with a balanced gold connector.",
-  },
-  {
     id: 4,
     slug: "xx99-mark-two-headphones",
     name: "XX99 Mark II Headphones",
@@ -43,7 +21,30 @@ export const headphones = [
     features:
       "Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening. It includes intuitive controls designed for any situation. Whether you’re taking a business call or just in your own personal space, the auto on/off and pause features ensure that you’ll never miss a beat.\n\nThe advanced Active Noise Cancellation with built-in equalizer allow you to experience your audio world on your terms. It lets you enjoy your audio in peace, but quickly interact with your surroundings when you need to. Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery life, the XX99 Mark II headphones gives you superior sound, cutting-edge technology, and a modern design aesthetic.",
   },
-  ,
+
+  {
+    id: 3,
+    slug: "xx99-mark-one-headphones",
+    name: "XX99 Mark I Headphones",
+    image: {
+      mobile: "/images/image-product-xx99-mark-one-mobile.jpg",
+      tablet: "/images/image-product-xx99-mark-one-tablet.jpg",
+      desktop: "/images/image-product-xx99-mark-one-desktop.jpg",
+    },
+    category: "headphones",
+    categoryImage: {
+      mobile: "/images/image-category-page-preview-xx99-mark-one-mobile.jpg",
+      tablet: "/images/image-category-page-preview-xx99-mark-one-tablet.jpg",
+      desktop: "/images/image-category-page-preview-xx99-mark-one-desktop.jpg",
+    },
+    new: false,
+    price: 1750,
+    description:
+      "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
+    features:
+      "As the headphones all others are measured against, the XX99 Mark I demonstrates over five decades of audio expertise, redefining the critical listening experience. This pair of closed-back headphones are made of industrial, aerospace-grade materials to emphasize durability at a relatively light weight of 11 oz.\n\nFrom the handcrafted microfiber ear cushions to the robust metal headband with inner damping element, the components work together to deliver comfort and uncompromising sound. Its closed-back design delivers up to 27 dB of passive noise cancellation, reducing resonance by reflecting sound to a dedicated absorber. For connectivity, a specially tuned cable is includes with a balanced gold connector.",
+  },
+
   {
     id: 2,
     slug: "xx59-headphones",
--- pages/earphones.tsx
+++ pages/earphones.tsx
@@ -1,10 +1,42 @@
-import { CategoryLayout } from "../components";
+import { CategoryLayout, Title } from "../components";
+import { earphones } from "../earphone";
+import Image from "next/image";
 
 const Earphones = () => {
   return (
     <section>
       <CategoryLayout title="earphones">
-        <h1>Earphones</h1>
+        <section className="px-6 mt-[4rem] md:mt-[7.5rem] lg:mt-[10rem]">
+          <div className="space-y-[7.5rem] pt-16 lg:max-w-[69.364rem] lg:mx-auto category overflow-hidden">
+            {earphones.map((earphone) => (
+              <Title
+                key={earphone.id}
+                title={earphone.name}
+                description={earphone.description}
+                isNew={earphone.new}
+              >
+                <Image
+                  src={earphone.categoryImage.mobile}
+                  alt={earphone.name}
+                  fill
+                  className="md:hidden object-cover"
+                />
+                <Image
+                  src={earphone.categoryImage.tablet}
+                  alt={earphone.name}
+                  fill
+                  className="hidden md:block lg:hidden object-cover"
+                />
+                <Image
+                  src={earphone.categoryImage.desktop}
+                  alt={earphone.name}
+                  fill
+                  className="hidden lg:block object-cover"
+                />
+              </Title>
+            ))}
+          </div>
+        </section>
       </CategoryLayout>
     </section>
   );
