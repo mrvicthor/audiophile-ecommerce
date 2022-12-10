df09c483224ca77d5c2ea51e1636962903a54f4f
mrvicthor <victoreleanya89@gmail.com>
2022-12-10 21:45:32 +0000

    updated cart

--- components/cart/index.tsx
+++ components/cart/index.tsx
@@ -1,6 +1,7 @@
 import { useCart } from "../../store";
 import Image from "next/image";
 import { Button } from "../index";
+import { it } from "node:test";
 
 const Cart = () => {
   const { cart, increaseQuantity, decreaseQuantity, emptyCart } = useCart();
@@ -17,25 +18,41 @@ const Cart = () => {
     maximumSignificantDigits: 4,
   });
 
+  const totalCost = cart.items
+    .map((item) => item.quantity * item.price)
+    .reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+
+  const numberOfItemsInCart = cart.items
+    .map((items) => items.quantity)
+    .reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
   return (
     <section className="px-6 fixed top-[7.125rem] z-50  left-0 right-0">
       <div className="max-h-[30.5rem] bg-[#ffffff]  rounded-md px-7 pb-8">
         {!cart.items.length ? (
-          <p>Cart is empty</p>
+          <p className="text-center pt-8 text-[#000000] font-bold leading-[1.5369rem]">
+            Cart is empty
+          </p>
         ) : (
           <>
             <div className="flex justify-between items-center pt-8 ">
               <h3 className="font-bold uppercase text-lg leading-[1.5369rem] tracking-[1.29px] text-[#000000]">
-                cart <span>({cart.items.length})</span>
+                cart <span>({numberOfItemsInCart})</span>
               </h3>
-              <button className="font-medium text-[#000000] opacity-50 leading-[1.5625rem]">
+              <button
+                className="font-medium text-[#000000] opacity-50 leading-[1.5625rem]"
+                onClick={() => emptyCart()}
+              >
                 Remove all
               </button>
             </div>
-            <div className="mt-8">
+            <div className="mt-8 flex flex-col gap-6">
               {cart.items.map((item) => (
                 <div
-                  className="flex gap-3 justify-between items-center"
+                  className="flex gap-2 justify-between items-center"
                   key={item.id}
                 >
                   <div className="h-16 w-16 relative">
@@ -46,7 +63,7 @@ const Cart = () => {
                       className="object-cover"
                     />
                   </div>
-                  <div className="w-[4.75rem]">
+                  <div className="w-[4.75rem] flex-1">
                     <h4 className="text-bold leading-[1.5625rem] text-[#000000]">
                       {removeLastString(item.name)}
                     </h4>
@@ -55,9 +72,13 @@ const Cart = () => {
                     </p>
                   </div>
                   <div className="h-8 w-[6rem] bg-[#f1f1f1] flex justify-between items-center px-4">
-                    <button>-</button>
+                    <button onClick={() => decreaseQuantity(item.slug)}>
+                      -
+                    </button>
                     <span>{item.quantity}</span>
-                    <button>+</button>
+                    <button onClick={() => increaseQuantity(item.slug)}>
+                      +
+                    </button>
                   </div>
                 </div>
               ))}
@@ -67,7 +88,7 @@ const Cart = () => {
                 total
               </p>
               <p className="text-[#000000] text-lg uppercase font-bold">
-                $2,455
+                {toUSDollar.format(totalCost)}
               </p>
             </div>
             <Button
