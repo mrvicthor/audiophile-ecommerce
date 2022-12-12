b8eecb37ad8b15f9d567fec0c9fb8ffcc53c9e0d
mrvicthor <victoreleanya89@gmail.com>
2022-12-12 06:21:32 +0000

    updated

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -6,12 +6,10 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useShowCart } from "../../hooks/use-Boolean";
-import { useToggle } from "usehooks-ts";
 
 type HeaderProps = {
   style?: string;
-  toggle: () => void;
+  toggle?: () => void;
 };
 
 const Header = ({ style, toggle }: HeaderProps) => {
