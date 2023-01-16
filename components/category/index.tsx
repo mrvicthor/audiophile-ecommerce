d064aeac0cca5c293d7ab52a745f86f54dfa2d10
mrvicthor <victoreleanya89@gmail.com>
2023-01-16 21:39:33 +0000

    updated

--- components/category/index.tsx
+++ components/category/index.tsx
@@ -1,10 +1,10 @@
 import { Speaker, SpeakerProduct, Earphone } from "../index";
 const Category = () => {
   return (
-    <section
-      className="my-[7.5rem] md:my-[6rem] lg:mt-[10.5rem] lg:mb-[12.5rem]"
-      role="menu"
-    >
+    <section className="my-[7.5rem] md:my-[6rem] lg:mt-[10.5rem] lg:mb-[12.5rem]">
+      <span className="sr-only" role="menubar">
+        Product gallery
+      </span>
       <Speaker />
       <SpeakerProduct />
       <Earphone />
